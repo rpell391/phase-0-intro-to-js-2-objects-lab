dfc1b2ee2afa0d681660836d8bcd8c89e9d3caa6
Randy Pellegrin <rpell391@gmail.com>
2022-05-18 00:37:29 -0500

    commit tree d865faeeb4f8d857f7a4fab6d2da644ff22fc7da

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const employee = {
+  name: "Randy",
+  streetAddress: "13110 pear",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value,
+  };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key, value) {
+  const newEmployee = { ...deleteFromEmployeeByKey.key };
+  newEmployee[key];
+  return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
